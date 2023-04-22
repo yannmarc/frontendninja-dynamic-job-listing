bcee550ba649a6b5e31dd8226e412f2961cfabe7
yannmarc <chamackstone@gmail.com>
2023-04-22 15:25:41 +0100

    added skeleton upon fetching data

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,32 +1,37 @@
 import React, { Fragment, useContext, useEffect, useState } from 'react';
 import JobCard from './JobCard';
-import useFetchJobs from '../hooks/useFetchJobs';
 import { EndPointContext } from '../App';
+import CardSkeleton from './CardSkeleton';
 
 
 const Main = () => {
     const {endPoint, setEndPoint} = useContext(EndPointContext);
-    console.log(endPoint);
     const [results, setReulsts] = useState([]);
-    console.log(results);
+    const [isLoaded, setLoaded] = useState(false);
 
     useEffect(() => {
       const fetchJobs = async (apiEndpoint) => {
           try {
               await fetch (apiEndpoint)
               .then((res) => res.json())
-              .then((data) => setReulsts(data))
+              .then((data) => {
+                setReulsts(data);
+                setLoaded(true);
+            })
           } catch (err) {
               console.log(err.message)
           }
       }
-      fetchJobs(endPoint);
-    }, [endPoint])
+      setTimeout(() => {
+        fetchJobs(endPoint);
+      }, 2000)
+    }, [endPoint, setEndPoint])
 
   return (
     <Fragment>
         <main>
-            <div className="px-6 pt-[55px] lg:max-w-4xl mx-auto xl:max-w-5xl">
+            <div className="px-6 pt-[55px] lg:max-w-4xl mx-auto xl:max-w-5xl lg:mt-[70px]">
+                {isLoaded ? null : <CardSkeleton />}
                 {results.map((job) => (<JobCard job={job} key={job.id}/>))}
             </div>
         </main>
